8ba4047c0cb880f445888221eed434eaea33cc57
OlivierGingras2 <2367082@cegepmontpetit.ca>
2025-01-30 14:27:07 -0500

    Ajout GamePurchase tout marche

--- Models/Game.cs
+++ Models/Game.cs
@@ -10,10 +10,14 @@ namespace RelationsNaN.Models
         public int ReleaseYear { get; set; }
 
         public int? GenreId { get; set; }
+
         [ValidateNever]
         public Genre? Genre { get; set; }
 
         [ValidateNever]
         public List<Platform> Platforms { get; set; }
+
+        [ValidateNever]
+        public List<GamePurchase> GamePurchases { get; set; }
     }
 }
